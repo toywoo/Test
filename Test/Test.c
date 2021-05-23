5f21e7f3053825b056925a2877c839f06675e1cd
Woo <toywoo02@naver.com>
2021-05-23 18:51:52 +0900

    Mul

--- Test/Test.c
+++ Test/Test.c
@@ -8,10 +8,15 @@ void NewSub(int i, int j)
 {
 	printf("%d", i + j);
 }
+void Mul(int a, int b)
+{
+	printf("%d", a * b);
+}
 
 void main()
 {
 	printf("Hello World");
 	NewAdd(3, 4);
 	NewSub(4, 3);
+	Mul(4, 3);
 }
