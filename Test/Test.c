d603e1db52ac8e1ff21c75ab660bc682c489b13e
Woo <toywoo02@naver.com>
2021-05-23 18:54:24 +0900

    ChangeNewSub

--- Test/Test.c
+++ Test/Test.c
@@ -4,19 +4,11 @@ void NewAdd(int a, int b)
 {
 	printf("%d", a + b);
 }
-void NewSub(int i, int j)
-{
-	printf("%d", i + j);
-}
-void Mul(int a, int b)
-{
-	printf("%d", a * b);
-}
+
 
 void main()
 {
 	printf("Hello World");
 	NewAdd(3, 4);
 	NewSub(4, 3);
-	Mul(4, 3);
 }
