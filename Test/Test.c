89bc633dbb126c09fb37cc5e36fb750b1fc6434e
Woo <toywoo02@naver.com>
2021-05-23 18:45:02 +0900

    NewSubFunction

--- Test/Test.c
+++ Test/Test.c
@@ -4,10 +4,14 @@ void NewAdd(int a, int b)
 {
 	printf("%d", a + b);
 }
-
+void NewSub(int i, int j)
+{
+	printf("%d", i + j);
+}
 
 void main()
 {
 	printf("Hello World");
 	NewAdd(3, 4);
+	NewSub(4, 3);
 }
