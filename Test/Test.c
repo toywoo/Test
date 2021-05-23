24ab3fef8dddd980593b844728d14073fe73160e
Woo <toywoo02@naver.com>
2021-05-23 18:58:59 +0900

    NewDiv

--- Test/Test.c
+++ Test/Test.c
@@ -2,11 +2,15 @@
 
 void NewAdd(int a, int b)
 {
-	printf("%d", a + b);
+	printf("%d\n", a + b);
 }
 void NewSub(int i, int j)
 {
-	printf("%d", i + j);
+	printf("%d\n", i + j);
+}
+void NewDiv(int i, int j)
+{
+	printf("%d", i / j);
 }
 
 void main()
@@ -14,4 +18,5 @@ void main()
 	printf("Hello World");
 	NewAdd(3, 4);
 	NewSub(4, 3);
+	NewDiv(4, 2);
 }
